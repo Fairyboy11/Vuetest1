2730252dd39b5298d79d1d93cb18b192ffcd62b9
Fairyboy11 <88618249+Fairyboy11@users.noreply.github.com>
2022-10-07 22:26:08 +0800

    Create .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
 
   // add your custom rules here
+// TEST
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
     "vue/max-attributes-per-line": [2, {
